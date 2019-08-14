a1d54fa925ee716759f4b77928b3285b3d64effa
Noi <noi.patsin@gmail.com>
2019-08-14 11:22:55 -0700

    finish setting local storage functionality

--- book-list/appes6.js
+++ book-list/appes6.js
@@ -58,6 +58,74 @@ class UI {
     }
 }
 
+/* =============== Local Storge ================== */
+class Store {
+    // fetch book from the local storage
+    static getBooks() {
+        // initualize a variable to hold books info - to use with other method
+        let books;
+        // check local storage
+        if(localStorage.getItem('books') === null) {
+            // if the books is no there - set it to an empty array
+            books = [];
+        } else {
+            // use JSON.parse - because we want it to be javascript object
+            books = JSON.parse(localStorage.getItem('books'));
+        }
+        return books;
+    }
+
+    // display books to the UI
+    static displayBooks() {
+        const books = Store.getBooks();
+
+        // loop throug boods with forEach
+        books.forEach(function(book) {
+            // instantiate the ui
+            const ui = new UI;
+
+            // Add book to Ui
+            ui.addBookToList(book);
+        })
+    }
+
+    // add books to the local storage
+    static addBook(book) {
+        // get book form local storage
+        const books = Store.getBooks();
+
+        books.push(book);
+
+        // set local storage with the new book
+        // use JSON.stringify - in order to put it in the local storage
+        // and becsuse we want to set the books as array
+        localStorage.setItem('books', JSON.stringify(books));
+    }
+
+    // remove books from the local storage
+    static removeBook(isbn) {
+        // need to find the unique object to target the remove
+        // because we don't have book's id - we are going to use isbn
+        // console.log(isbn);
+
+        // get the book from the local storage
+        const books = Store.getBooks();
+
+        // loop through with forEach
+        books.forEach(function(book, index) {
+            if (book.isbn === isbn) {
+                books.splice(index, 1);
+            }
+        })
+        localStorage.setItem('books', JSON.stringify(books));
+    }
+}
+
+/* =============== DOM Load Event ================== */
+// call books in the local storage
+document.addEventListener('DOMContentLoaded', Store.displayBooks);
+
+/* =============== Event Listeners ================== */
 // 2. Event Listeners for add book
 document.getElementById('book-form').addEventListener('submit', function(e) {
     // console.log('test')
@@ -83,6 +151,8 @@ document.getElementById('book-form').addEventListener('submit', function(e) {
     } else {
         // Add book to list
         ui.addBookToList(book);
+        // Add to local storage
+        Store.addBook(book);
 
         // Show success
         ui.showAlert('Book Added!', 'success');
@@ -103,6 +173,8 @@ document.getElementById('book-list').addEventListener('click', function(e) {
 
     // Delete book
     ui.deleteBook(e.target);
+    // Remove book from local storage by targeting isbn
+    Store.removeBook(e.target.parentElement.previousElementSibling.textContent);
 
     // Show message
     ui.showAlert('Book Removed!', 'success');
